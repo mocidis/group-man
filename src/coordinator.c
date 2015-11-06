86dd850c11ac5b7e7e3e017d601224231a8b4fda
buidanhquy <quy.buidanhquy@gmail.com>
2015-11-06 10:37:01 +0700

    update riuc4

--- src/coordinator.c
+++ src/coordinator.c
@@ -151,21 +151,34 @@ void on_request(gm_server_t *gm_server, gm_request_t *request) {
             gmc_client_send(&temp2->gmc_client, &gmc_req);
             break;
         case GM_INFO:
-            SHOW_LOG(4, fprintf(stdout, "Receive request: ID=%d\n", request->msg_id));
+            SHOW_LOG(4, fprintf(stdout, "%s receive request from %s ID=%d\n", request->gm_info.gm_owner, request->gm_info.gm_guest, request->msg_id));
 
-            temp = find_entry_by_id(request->gm_info.info_id);
+            temp = find_entry_by_id(request->gm_info.gm_owner);
             if (temp == NULL) {
-                SHOW_LOG(4, fprintf(stdout,"Error ID not found!\n"));
+                SHOW_LOG(4, fprintf(stdout,"Error Owner ID not found for %s!\n", request->gm_group.owner));
                 break;
+            }
+            else {
+                SHOW_LOG(4, fprintf(stdout, "Onwer: %s\n", temp->id));
+            }
+
+            temp2 = find_entry_by_id(request->gm_group.guest);
+            if (temp2 == NULL) {
+                SHOW_LOG(4, fprintf(stdout,"Error Guest ID not found for %s!\n", request->gm_group.guest));
+                break;
+            }
+            else {
+                SHOW_LOG(4, fprintf(stdout, "Guest: %s\n", temp2->id));
             }
 
             adv_request_t req;
             req.msg_id = ADV_INFO;
-            ansi_copy_str(req.adv_info.info_id, request->gm_info.info_id);
+            ansi_copy_str(req.adv_info.adv_owner, request->gm_info.gm_owner);
+            ansi_copy_str(req.adv_info.adv_guest, request->gm_info.gm_guest);
             ansi_copy_str(req.adv_info.sdp_mip, request->gm_info.sdp_mip);
             req.adv_info.sdp_port = request->gm_info.sdp_port;
        
-            SHOW_LOG(4, fprintf(stdout, "Send ADV info from %s to addr %s\n", temp->id, temp->adv_client.connect_str));     
+            SHOW_LOG(4, fprintf(stdout, "Send ADV info from %s to %s Addr %s\n", temp->id, temp2->id, temp2->adv_client.connect_str));     
             adv_client_send(&temp->adv_client, &req);
             break;
         default:
