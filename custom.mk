d7b1564b571fa5370161f1190cbd5edc41f3c788
Tung HOANG <tung.hoang@gmail.com>
2016-01-25 09:33:27 +0700

    update custom.mk for building on MacOS X

--- custom.mk
+++ custom.mk
@@ -27,4 +27,6 @@ endif
 ifeq ($(CROSS_COMPILE),$(MACOS_X86_64))
 	CROSS_TOOL:=gcc
 	LIBS_DIR:=../libs/darwin-x86_64
+    LIBS:= -L$(LIBS_DIR)/lib -ljson-c -lpjsua2-x86_64-apple-darwin12.5.0 -lstdc++ -lpjsua-x86_64-apple-darwin12.5.0 -lpjsip-ua-x86_64-apple-darwin12.5.0 -lpjsip-simple-x86_64-apple-darwin12.5.0 -lpjsip-x86_64-apple-darwin12.5.0 -lpjmedia-codec-x86_64-apple-darwin12.5.0 -lpjmedia-x86_64-apple-darwin12.5.0 -lpjmedia-videodev-x86_64-apple-darwin12.5.0 -lpjmedia-audiodev-x86_64-apple-darwin12.5.0 -lpjmedia-x86_64-apple-darwin12.5.0 -lpjnath-x86_64-apple-darwin12.5.0 -lpjlib-util-x86_64-apple-darwin12.5.0 -lsrtp-x86_64-apple-darwin12.5.0 -lresample-x86_64-apple-darwin12.5.0 -lgsmcodec-x86_64-apple-darwin12.5.0 -lspeex-x86_64-apple-darwin12.5.0 -lilbccodec-x86_64-apple-darwin12.5.0 -lg7221codec-x86_64-apple-darwin12.5.0 -lportaudio-x86_64-apple-darwin12.5.0 -lpj-x86_64-apple-darwin12.5.0 -lm -lpthread 
+#-framework CoreAudio -framework CoreServices -framework AudioUnit -framework AudioToolbox -framework Foundation -framework AppKit -framework QTKit -framework QuartzCore -framework OpenGL
 endif
